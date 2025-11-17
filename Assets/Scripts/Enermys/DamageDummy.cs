02837fd9db4a3d4ecc0f3dfaf0d13f116b0d34f3
Martyn Elwell <martyn.elwell@btopenworld.com>
2025-11-17 10:51:51 +0000

    Target dummies made

--- Assets/Scripts/Enermys/DamageDummy.cs
+++ Assets/Scripts/Enermys/DamageDummy.cs
@@ -3,11 +3,18 @@ using UnityEngine;
 public class DamageableDummy : MonoBehaviour, IDamageable
 {
     public float health = 100f;
+    public GameObject floatingText;
 
+    void Start()
+    {
+        TakeDamage(20);
+    }
     public void TakeDamage(float amount)
     {
         health -= amount;
-        Debug.Log($"{gameObject.name} took {amount} damage! Remaining HP: {health}");
+        GameObject Text = Instantiate(floatingText, transform.position, Quaternion.identity);
+        if (Text.GetComponent<FloatingText>())
+            Text.GetComponent<FloatingText>().SetText(amount.ToString(), false);
 
         if (health <= 0f)
         {
