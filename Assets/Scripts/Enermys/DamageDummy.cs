d2902995cc754a5dd817a89b9de4749daf87f514
Martyn Elwell <martyn.elwell@btopenworld.com>
2025-11-18 14:17:49 +0000

    Enemy code structure

--- Assets/Scripts/Enermys/DamageDummy.cs
+++ Assets/Scripts/Enermys/DamageDummy.cs
@@ -1,25 +1,10 @@
 using UnityEngine;
 
-public class DamageableDummy : MonoBehaviour, IDamageable
+public class DamageableDummy : Enemy
 {
-    public float health = 100f;
-    public GameObject floatingText;
-
     void Start()
     {
         TakeDamage(20);
-    }
-    public void TakeDamage(float amount)
-    {
-        health -= amount;
-        GameObject Text = Instantiate(floatingText, transform.position, Quaternion.identity);
-        if (Text.GetComponent<FloatingText>())
-            Text.GetComponent<FloatingText>().SetText(amount.ToString(), false);
-
-        if (health <= 0f)
-        {
-            Destroy(gameObject);
-        }
     }
 }
 
